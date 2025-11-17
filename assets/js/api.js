45c83d5a345fb9822bf831ec454968598e943117
santanar2 <santana.r2d2@gmail.com>
2025-11-17 11:36:44 -0300

    alteração de url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,5 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://github.com/santanar2/js-developer-portfolio/tree/mainon';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
